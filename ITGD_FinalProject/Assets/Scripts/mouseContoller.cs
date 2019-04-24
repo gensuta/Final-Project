d379459353ab8fd09a0a4d0735965f48d7673626
Geneva Heyward <geneva.heyward@urbanarts.org>
2019-04-24 12:26:38 -0400

    Began working on cut wood minigame. It works! There's comments for each of the scripts in the scene etc. Strong tree sprite is in the game. Trees fade in/out if it's the current one or if it's dead or if it's waiting to be chopped.
    Will work on distance detection next

---
+++ ITGD_FinalProject/Assets/Scripts/mouseContoller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class mouseContoller : MonoBehaviour
+{
+    public float timer;
+
+    public int treesCut; // the number of trees cut down
+    public GameObject currentTree_OBJ; // the current tree we're cutting down
+    public treeScript currentTree_SCR; // the current tree's script
+
+    treeManager tM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tM = FindObjectOfType<treeManager>(); // we need this to access that handy dandy list!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tM.treeScripts.Count > treesCut)
+        {
+
+            if (tM.treeScripts[treesCut] != null)
+            {
+                tM.treeScripts[treesCut].isCurrentTree = true;
+            }
+
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //getting where your mouse is in the actual game
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y); // turning that vector 3 into a vector 2 for raycasting sake
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero); //storing what we may have hit via raycasting!
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject == tM.treeObjects[treesCut])
+                {
+                    tM.treeScripts[treesCut].treeHP -= 1;
+                }
+
+                //treesCut += 1;
+                //Debug.Log(hit.collider.gameObject.name + " " + treesCut.ToString());
+            }
+        }
+    }
+}
---
+++ ITGD_FinalProject/Assets/Scripts/treeScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treeScript : MonoBehaviour
+{
+    public int treeType; // if its a weak or strong tree. Or if we decide to add more tree types
+                         // 0 is weak, 1 is strong
+
+    public int treeNum; //where is this tree in the treeManager list? The first or the last tree?
+    public bool isCurrentTree; // to test if our boxcollider is active!
+
+    BoxCollider2D bc; // the tree's box collider
+    SpriteRenderer sp; //the tree's sprite renderer
+
+    public int treeHP; // how many times must I be slashed by the mouse? 
+
+
+    public Color currentTree_COL; // if you're the current tree yr clear!
+    public Color notCurrent_COL; // if you're not the current tree you're slightly desaturated
+    public Color deathColor; // if the tree is gone
+
+    public float changeColor_SPD; // the speed at which the trees color changes
+
+    mouseContoller mC;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mC = FindObjectOfType<mouseContoller>();
+        bc = GetComponent<BoxCollider2D>();
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCurrentTree)
+        {
+            bc.enabled = true;
+            if (sp.color != currentTree_COL)
+            {
+                sp.color = Color.Lerp(currentTree_COL, sp.color, changeColor_SPD);
+            }
+        }
+
+        else
+        {
+            bc.enabled = false;
+            if (sp.color != notCurrent_COL)
+            {
+                sp.color = Color.Lerp(notCurrent_COL, sp.color, changeColor_SPD);
+            }
+        }
+
+        if (treeHP <= 0)
+        {
+            mC.treesCut += 1;
+            if (sp.color != deathColor)
+            {
+                sp.color = Color.Lerp(deathColor, sp.color, changeColor_SPD);
+            }
+            Destroy(gameObject,1f);
+        }
+    }
+
+    public void InitializeTree(int num, int type)
+    {
+        treeNum = num; // this is what order the tree is! bigger is farther back
+        sp.sortingOrder = -num; // we make num negative so that the bigger nums are further back instead of further forward
+
+        if (type == 0) // weak trees have 1 hp
+        {
+            treeHP = 1;
+        }
+        else if (type == 1) // strong trees have 2 hp
+        {
+            treeHP = 2;
+        }
+    }
+}
