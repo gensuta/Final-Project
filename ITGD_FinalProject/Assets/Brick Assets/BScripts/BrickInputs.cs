1ccd38141954c01554b0d970b729ff165275b2bc
Justin <justintrongvo@gmail.com>
2019-04-24 12:40:59 -0400

    brickgamestuff

---
+++ ITGD_FinalProject/Assets/Brick Assets/BScripts/BrickInputs.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickInputs : MonoBehaviour
+{
+    public int phase = 1;
+    public bool isTop = true;
+    public bool brickDone = true;
+
+    public int topRow;
+    public int botRow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        topRow = Random.Range(0, 3);
+        botRow = Random.Range(0, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (topRow == 0 && isTop)
+        {
+            if (phase == 1 && isTop && Input.GetKeyDown(KeyCode.Q))
+            {
+                phase = 2; 
+            }
+            if (phase == 2 && isTop && Input.GetKeyDown(KeyCode.W))
+            {
+                phase = 3;
+            }
+            if (phase == 3 && isTop && Input.GetKeyDown(KeyCode.E))
+            {
+                phase = 4;
+                isTop = false;
+            }
+        }
+        if (topRow == 1 && isTop)
+        {
+            if (phase == 1 && isTop && Input.GetKeyDown(KeyCode.I))
+            {
+                phase = 2;
+            }
+            if (phase == 2 && isTop && Input.GetKeyDown(KeyCode.O))
+            {
+                phase = 3;
+            }
+            if (phase == 3 && isTop && Input.GetKeyDown(KeyCode.P))
+            {
+                phase = 4;
+                isTop = false;
+            }
+        }
+        if (topRow == 2 && isTop)
+        {
+            if (phase == 1 && isTop && Input.GetKeyDown(KeyCode.Z))
+            {
+                phase = 2;
+            }
+            if (phase == 2 && isTop && Input.GetKeyDown(KeyCode.X))
+            {
+                phase = 3;
+            }
+            if (phase == 3 && isTop && Input.GetKeyDown(KeyCode.C))
+            {
+                phase = 4;
+                isTop = false;
+            }
+        }
+        if (topRow == 3 && isTop)
+        {
+            if (phase == 1 && isTop && Input.GetKeyDown(KeyCode.B))
+            {
+                phase = 2;
+            }
+            if (phase == 2 && isTop && Input.GetKeyDown(KeyCode.N))
+            {
+                phase = 3;
+            }
+            if (phase == 3 && isTop && Input.GetKeyDown(KeyCode.M))
+            {
+                phase = 4;
+            }
+        }
+
+        if (phase == 4 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            isTop = false;
+            phase = 5;
+        }
+
+    }
+
+    void CutWood()
+    {
+        topRow = Random.Range(0, 3);
+        botRow = Random.Range(0, 3);
+    }
+}
---
+++ ITGD_FinalProject/Assets/Brick Assets/BScripts/InputPlace.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputPlace : MonoBehaviour
+{
+    BrickInputs bri;
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+        bri = FindObjectOfType<BrickInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bri.phase == 4)
+        {
+            text.text = "SPACE";
+        }
+
+        if (bri.topRow == 0 && bri.isTop == true && bri.phase == 1)
+        {
+            text.text = "Q W E";
+        }
+        if (bri.topRow == 0 && bri.isTop == true && bri.phase == 2)
+        {
+            text.text = "W E";
+        }
+        if (bri.topRow == 0 && bri.isTop == true && bri.phase == 3)
+        {
+            text.text = "E";
+        }
+
+
+        if (bri.topRow == 1 && bri.isTop == true && bri.phase == 1)
+        {
+            text.text = "I O P";
+        }
+        if (bri.topRow == 1 && bri.isTop == true && bri.phase == 2)
+        {
+            text.text = "O P";
+        }
+        if (bri.topRow == 1 && bri.isTop == true && bri.phase == 3)
+        {
+            text.text = "P";
+        }
+
+
+        if (bri.topRow == 2 && bri.isTop == true && bri.phase == 1)
+        {
+            text.text = "Z X C";
+        }
+        if (bri.topRow == 2 && bri.isTop == true && bri.phase == 2)
+        {
+            text.text = "X C";
+        }
+        if (bri.topRow == 2 && bri.isTop == true && bri.phase == 3)
+        {
+            text.text = "C";
+        }
+
+
+        if (bri.topRow == 3 && bri.isTop == true && bri.phase == 1)
+        {
+            text.text = "B N M";
+        }
+        if (bri.topRow == 3 && bri.isTop == true && bri.phase == 2)
+        {
+            text.text = "N M";
+        }
+        if (bri.topRow == 3 && bri.isTop == true && bri.phase == 3)
+        {
+            text.text = "M";
+        }
+
+
+
+
+        if (bri.botRow == 0 && bri.isTop == true && bri.phase == 5)
+        {
+            text.text = "Q W E";
+        }
+        if (bri.botRow == 0 && bri.isTop == true && bri.phase == 6)
+        {
+            text.text = "W E";
+        }
+        if (bri.botRow == 0 && bri.isTop == true && bri.phase == 7)
+        {
+            text.text = "E";
+        }
+
+
+        if (bri.botRow == 1 && bri.isTop == true && bri.phase == 5)
+        {
+            text.text = "I O P";
+        }
+        if (bri.botRow == 1 && bri.isTop == true && bri.phase == 6)
+        {
+            text.text = "O P";
+        }
+        if (bri.botRow == 1 && bri.isTop == true && bri.phase == 7)
+        {
+            text.text = "P";
+        }
+
+
+        if (bri.botRow == 2 && bri.isTop == true && bri.phase == 5)
+        {
+            text.text = "Z X C";
+        }
+        if (bri.botRow == 2 && bri.isTop == true && bri.phase == 6)
+        {
+            text.text = "X C";
+        }
+        if (bri.botRow == 2 && bri.isTop == true && bri.phase == 7)
+        {
+            text.text = "C";
+        }
+
+
+        if (bri.botRow == 3 && bri.isTop == true && bri.phase == 5)
+        {
+            text.text = "B N M";
+        }
+        if (bri.botRow == 3 && bri.isTop == true && bri.phase == 6)
+        {
+            text.text = "N M";
+        }
+        if (bri.botRow == 3 && bri.isTop == true && bri.phase == 7)
+        {
+            text.text = "M";
+        }
+    }
+}
