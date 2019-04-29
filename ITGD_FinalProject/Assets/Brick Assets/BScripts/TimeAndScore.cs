6a807e53fdca242a2cf519b14a73c562dfcad733
Justin <justintrongvo@gmail.com>
2019-04-29 12:48:21 -0400

    brick game w scoring

---
+++ ITGD_FinalProject/Assets/Brick Assets/BScripts/TimeAndScore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeAndScore : MonoBehaviour
+{
+    Text text;
+    static public float secs;
+
+
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+        text.text = "0";
+
+    }
+
+    void Update()
+    {
+        // Press the space key to change the Text message.
+        if (text.tag == "score")
+        {
+            text.text = "Score: " + GameObject.Find("BrickManager").GetComponent<BrickInputs>().brickCount;
+        }
+        secs += Time.deltaTime;
+        if (text.tag == "timer")
+        {
+            text.text = secs.ToString("#.0");
+        }
+    }
+}
